cb738d75f8fb275f139ccd0970195b1813a58fab
xixi0224 <2663955118qq.com>
2026-04-29 17:15:17 +0800

    Fix environment variable names for Railway MySQL

--- app/config.py
+++ app/config.py
@@ -1,11 +1,11 @@
 import os
 
 DB_CONFIG = {
-    "host": os.environ.get("MYSQL_HOST", "127.0.0.1"),
-    "port": int(os.environ.get("MYSQL_PORT", "3306")),
-    "user": os.environ.get("MYSQL_USER", "root"),
-    "password": os.environ.get("MYSQL_PASSWORD", "041602"),
-    "database": os.environ.get("MYSQL_DATABASE", "zhinote"),
+    "host": os.environ.get("MYSQLHOST", os.environ.get("MYSQL_HOST", "127.0.0.1")),
+    "port": int(os.environ.get("MYSQLPORT", os.environ.get("MYSQL_PORT", "3306"))),
+    "user": os.environ.get("MYSQLUSER", os.environ.get("MYSQL_USER", "root")),
+    "password": os.environ.get("MYSQLPASSWORD", os.environ.get("MYSQL_PASSWORD", "041602")),
+    "database": os.environ.get("MYSQLDATABASE", os.environ.get("MYSQL_DATABASE", "zhinote")),
     "charset": "utf8mb4",
 }
 
